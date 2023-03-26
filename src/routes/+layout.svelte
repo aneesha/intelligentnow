c6de7bbf66e35f707d9b4de3d8b2192ee1628ea0
aneesha <aneesha.bakharia@gmail.com>
2023-03-26 10:57:14 +1000

    First commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import './pico.slim.min.css';
+	
+</script>
+
+<div class="app">
+
+	<main class="container">
+		<slot />
+	</main>
+
+</div>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+
+<script>
+    import materialism_logo from '$lib/images/materialism.png';
+</script>
+
+<svelte:head>
+	<title>Intelligent Now</title>
+	<meta name="description" content="Expositions with AI." />
+</svelte:head>
+
+
+        <hgroup>
+            <h1>Intelligent Now!</h1>
+            <p>A series of expositions with AI. Some ART, some PRACTICAL, some EDUCTIONAL...</p>
+        </hgroup>
+
+
+        <!-- Article-->
+        <article id="article">
+            <hgroup>
+                <h3>Materialism</h3>
+                <p>A graphic novel with an original story made entirely with AI generated images using Stable Diffusion 1.5 and 2. Put together in Comic Life.</p>
+
+            </hgroup>
+
+            <p><a href="https://drive.google.com/file/d/1TsxJTWeocx_a91TpKXE5lHzoH3SJg6-u/view"><img src={materialism_logo} width="25%" alt="Cover photo for Materialism Graphic Novel." /></a></p>
+
+            <p>
+                <a href="https://drive.google.com/file/d/1TsxJTWeocx_a91TpKXE5lHzoH3SJg6-u/view" role="button" class="secondary">Read</a>
+            </p>
+            
+        </article>
+        <!-- ./ Article-->
+
+        <details>
+            <summary>About this site</summary>
+            <p>Intelligent Now is made by <a href="https://www.twitter.com/aneesha">@aneesha</a> and a way to showcase everything she is buildng in partnership with AI.</p>
+        </details>
